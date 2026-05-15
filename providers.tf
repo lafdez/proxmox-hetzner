3e81e5e7460cc20c3fb3aa40f83764feaac3dcff
Luis Ángel Fernández Fernández <la@zzsoft.es>
2026-05-15 09:44:41 +0200

    Provider and variables used so far

---
+++ providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "Telmate/proxmox"
+      version = "3.0.2-rc07"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url          = var.api_url
+  pm_api_token_id     = var.token_id
+  pm_api_token_secret = var.token_secret
+  pm_tls_insecure     = false
+}
+
---
+++ variables.tf
@@ -0,0 +1,77 @@
+variable "api_url" {
+  type = string
+}
+
+variable "token_id" {
+  type = string
+}
+
+variable "token_secret" {
+  type = string
+}
+
+variable "tls_insecure" {
+  type = bool
+}
+
+variable "vm_ssh_key" {
+  type = string
+}
+
+variable "minecraft_vm_name" {
+  type = string
+}
+
+variable "minecraft_vm_desc" {
+  type = string
+}
+
+variable "minecraft_vm_target_node" {
+  type = string
+}
+
+variable "minecraft_vm_clone" {
+  type = string
+}
+
+variable "minecraft_vm_ipconfig0" {
+  type = string
+}
+
+variable "minecraft_vm_cpu_cores" {
+  type = number
+}
+
+variable "minecraft_vm_cpu_sockets" {
+  type = number
+}
+
+variable "minecraft_vm_memory" {
+  type = number
+}
+
+variable "minecraft_vm_disk_size" {
+  type = string
+}
+
+variable "minecraft_vm_disk_type" {
+  type = string
+}
+
+variable "minecraft_vm_disk_storage" {
+  type = string
+}
+
+variable "minecraft_vm_disk_slot" {
+  type = string
+}
+
+variable "minecraft_vm_bootdisk" {
+  type = string
+}
+
+variable "minecraft_vm_boot" {
+  type = string
+}
+
+
