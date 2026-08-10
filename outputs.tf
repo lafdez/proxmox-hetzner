c2138f12a246f0c2ef9f85b1848921fd5503cf8d
Luis Ángel Fernández Fernández <la@zzsoft.es>
2026-08-10 17:22:18 +0200

    Muestro la IP de la máquina de Satisfactory

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,10 @@
-output "vm_ip_address" {
+output "minecraft_vm_ip_address" {
   description = "The IP address of the Minecraft VM"
   value       = proxmox_vm_qemu.minecraft-vm.ipconfig0
 }
+
+output "satisfactory_vm_ip_address" {
+  description = "The IP address of the Satisfactory VM"
+  value       = proxmox_vm_qemu.satisfactory-vm.ipconfig0
+}
+
